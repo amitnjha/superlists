4407b03e387a0ee5886350da3bfac5c88cbee86d
Amit Jha <amitnjha@gmail.com>
2020-05-30 11:41:19 -0500

    page 64

--- test_functional_tests.py
+++ test_functional_tests.py
@@ -43,12 +43,19 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
 
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
-            )
-        self.fail('Finish the test!')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        #self.fail('Finish the test!')
         #There is still a text box inviting her to add another item.She enters "User peacock feathers to make a fly" (Edith is very methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: USe peocock feathers to make a fly', [row.text for row in rows])
+        
         #The page updates again and now shows both iterms on her list.
 
         #Edith wonders wherte the site will remember her list. Then she sees that the site has generated a unique URL for her -- There is some explanatory text to that effect.
