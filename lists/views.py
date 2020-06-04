71db3432ceba401b2adf90e93a57454c5be94b73
Amit Jha <amitnjha@gmail.com>
2020-06-04 04:19:17 -0500

    chapter 13 page 237

--- lists/views.py
+++ lists/views.py
@@ -12,7 +12,16 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id = list_id)
-    return render(request, 'list.html', {'list': list_})
+    error = None
+    if request.method == 'POST':
+        item = Item(text = request.POST['item_text'], list = list_)
+        try:
+            item.full_clean()
+            item.save()
+            return redirect(f'{settings.BASE_URL}/lists/{list_.id}/')
+        except ValidationError:
+            error  = "You can't have an empty list item"
+    return render(request, 'list.html', {'list': list_, 'error':error})
 
 def new_list(request):
    new_item_text = request.POST['item_text']
@@ -27,9 +36,9 @@ def new_list(request):
    
    return redirect(f'{settings.BASE_URL}/lists/{list_.id}/')
 
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    new_item_text = request.POST['item_text']
-    Item.objects.create(text = new_item_text, list = list_)
-    return redirect(f'{settings.BASE_URL}/lists/{list_.id}/')
+#def add_item(request, list_id):
+#    list_ = List.objects.get(id=list_id)
+#    new_item_text = request.POST['item_text']
+#    Item.objects.create(text = new_item_text, list = list_)
+#    return redirect(f'{settings.BASE_URL}/lists/{list_.id}/')
 
